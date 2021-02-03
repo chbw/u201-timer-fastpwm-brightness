5cc6abc41b9bb897a7e9d4c2cacee174c2e8a377
Christoph B. Wurzinger <wu@htlweiz.at>
2021-02-03 10:08:41 +0100

    changed duty cycle once again

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  * Author : user
  *
  * Control the brightness of a LED connected to PD3 (OC2B) with Fast PWM.
- * Set the duty cycle to approx 12.5%.
+ * Set the duty cycle to approx 25%.
  * Compare to a constantly lit LED connected to PD2.
  * 
  */ 
