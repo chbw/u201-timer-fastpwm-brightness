1c8857026e353c93a228e112063b1a508bb1c551
Christoph B. Wurzinger <wu@htlweiz.at>
2021-02-03 09:00:02 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+ * u201-timer-fastpwm-brightness.cpp
+ *
+ * Created: 2021-02-03 08:40:11
+ * Author : user
+ *
+ * Control the brightness of a LED connected to PD3 (OC2B) with Fast PWM.
+ * Set the duty cycle to approx 25%.
+ * Compare to a constantly lit LED connected to PD2.
+ * 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
